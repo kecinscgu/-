2dd55b68aeffafa635f50b4c2d47c7fd8b4ef0b3
zefeiO <ouzefei1551@gmail.com>
2021-07-05 22:18:00 +0800

    developing

--- c++/main.cpp
+++ c++/main.cpp
@@ -6,70 +6,126 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <string>
+#include <thread>
+#include <vector>
+
+
+class Connection
+{
+public:
+    
+    int _clientSocket;
+    std::vector<char> *_pHeader;
+    std::vector<char> _hostIp;
+    std::vector<char> _hostPort;
+    
+    
+    Connection(int clientSocket);
+}
+
+
+int setupProxy(const std::string *p_ip , int port)
+{
+    int proxySocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in proxyAddr;
+    inet_pton(AF_INET, (*p_ip).c_str(), &proxyAddr.sin_addr);
+    proxyAddr.sin_port = htons(port);
+    proxyAddr.sin_family = AF_INET;
+    
+    bind(proxySocket, AF_INET, (struct sockaddr *)&proxyAddr, sizeof(proxyAddr));
+
+    listen(proxySocket, SOMAXCONN);
+
+    std::cout << "http proxy server running at " << *p_ip << ":" << port << std::endl;
+
+    return proxySocket;
+}
+
+
+int acceptClient(int proxySocket)
+{
+    struct sockaddr_in client;
+    socklen_t clientSize = sizeof(client);
+    
+    int clientSocket = accept(proxySocket, (struct sockaddr *)&client, &clientSize);
+    return 0;
+}
+
+
+
+
+
+bool endsWith (std::vector<char> const& fullString, std::vector<char> const& ending) {
+    if (fullString.size() >= ending.size()) {
+        size_t pos = fullString.size() - ending.size();
+        for (size_t i{pos}; i < fullString.size(); i++)
+        {
+            if (fullString[i] != ending[i-pos])
+            {
+                return false;
+            }
+        }
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void handleConn(int clientSocket)
+{
+    Connection conn(clientSocket);
+    if (!*(conn._pHeader).size())
+    {
+        close(clientSocket);
+    }
+
+    
+    return;
+}
 
 int main()
 {
-    int proxySocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (proxySocket == -1)
-    {
-        std::cerr << "Can't create a socket!";
-        return -1;
-    }
-
-    sockaddr_in hint;
-    hint.sin_family = AF_INET;
-    hint.sin_port = htons(54000);
-    inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);
-
-    if (bind(proxySocket, AF_INET, (sockaddr*)&hint, sizeof(hint)) == -1)
-    {
-        std::cerr << "Can't bind to IP/port";
-        return -2;
-    }
-
-    if (listen(proxySocket, SOMAXCONN) == -1)
-    {
-        std::cerr << "Can't listen!";
-        return -3;
-    }
-
-    sockaddr_in client;
-    socklen_t clientSize = sizeof(client);
-    char host[NI_MAXHOST]{};
-    char service[NI_MAXSERV];
-    memset(host, 0, NI_MAXHOST);
-    memset(service, 0, NI_MAXSERV);
-
-    int clientSocket = accept(proxySocket, (sockaddr*)&client, &clientSize)
-    if (clientSocket == -1)
-    {
-        std::cerr << "Problem with client connecting";
-        return -4;
-    }
-
-    char buf[4096];
-    while (True)
-    {
-        memset(buf, 0, 4096);
-
-        int nBytesRecv = recv(clientSocket, buf, 4096, 0);
-        if (nBytesRecv == -1)
-        {
-            std::cerr << "There was a connection issue" << std::endl;
-            break;
-        }
-        if (nBytesRecv == 0)
-        {
-            std::cout << "The client disconnected" << std::endl;
-            break;
-        }
-        std::cout << "Received: " << std::string(buf, 0, nBytesRecv) << std::endl;
-
-        send(clientSocket, buf, nBytesRecv + 1, 0);
-    }
+    // setup a listening socket for the proxy server
+    std::string p = "127.0.0.1";
+    int port = 3000;
+    int proxySocket = setupProxy(&p, port);
     
-    close(proxySocket);
-
+    // wait for incoming client connectioins
+    while (true)
+    {
+        int clientSocket = acceptClient(proxySocket);
+        std::thread conn(handleConn, clientSocket);
+    }
 
     return 0;
+}
+
+
+Connection::Connection(int clientSocket):
+_clientSocket{clientSocket}
+{
+    std::vector<char> *p_header = new std::vector<char>;
+    char buf[4096];
+    memset(buf, 0, 4096);
+    while (true)
+    {
+        int nBytesRecv = recv(_clientSocket, buf, 4096, 0);
+        for (int i{0}; i < nBytesRecv; i++)
+        {
+            (*p_header).push_back(buf[i]);
+        }
+
+        std::vector<char> ending{13, 10, 13, 10}; // "/r/n/r/n"
+        if (endsWith(*p_header, ending) || nBytesRecv == 0)
+        {
+            break;
+        }
+
+        memset(buf, 0, 4096);
+    }
+
+    _pHeader = p_header;
+    _hostIp{};
+    _hostPort{};
 }
